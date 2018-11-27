f50c631afcd7e69301cf506920e88b7e5a309058
Nialota <intencia96@gmail.com>
2018-11-27 20:27:02 +0300

    ограничение доступа для неавторизованных

--- RecSystem/RecSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ RecSystem/RecSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -37,6 +37,7 @@ namespace RecSystem.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+
             [Required]
             [EmailAddress]
             public string Email { get; set; }
