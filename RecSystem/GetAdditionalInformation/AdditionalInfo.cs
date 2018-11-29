d76ea95998e2fd465d9e95fdaf9a89637221af2b
Dante <aleksandrov.a@roskvarta.ru>
2018-11-29 17:45:44 +0300

    Утилита по добавлению постеров.

---
+++ RecSystem/GetAdditionalInformation/AdditionalInfo.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+using RecSystem.Data;
+using System;
+using System.Text.RegularExpressions;
+
+namespace getadditionalinformation
+{
+    class additionalinfo
+    {
+        private ApplicationDbContext _context;
+
+        public additionalinfo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void insertUrl()
+        {
+            string urlBase = "https://www.rottentomatoes.com/m/";
+            string addPartPath;
+            foreach (var i in _context.Items)
+            {
+                addPartPath = i.MovieTitle;
+                Regex.Replace(addPartPath, "\s(\d*)", "");
+                addPartPath = addPartPath + " ";
+
+            }
+        }
+    }
+}
---
+++ RecSystem/GetAdditionalInformation/DoStuffService.cs
@@ -0,0 +1,22 @@
+﻿using RecSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetAdditionalInformation
+{
+    class DoStuffService
+    {
+        private ApplicationDbContext _context;
+        
+        public DoStuffService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void DoStuff()
+        {
+            Console.WriteLine("HUI");
+        }
+    }
+}
