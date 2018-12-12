d772e6a0d84d33a86aa40d4154f9a15d75656773
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-12-12 22:31:30 +0300

    add script, fix layout and site.css

--- RecSystem/RecSystem/wwwroot/js/site.js
+++ RecSystem/RecSystem/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var x = document.getElementsByClassName("navbar_item_link");
 
-// Write your JavaScript code.
+for (i = 0; i < x.length; i++) {
+    //console.log(x[i].innerHTML);
+    if (x[i].innerHTML == document.title) {
+
+        x[i].style.borderBottom = "5px solid #ECD4A0";
+        x[i].style.paddingBottom = "5px";
+    }
+}
