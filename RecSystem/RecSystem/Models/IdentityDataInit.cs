2d94f0365247c5835b80226788096d464a88a7d5
Nialota <intencia96@gmail.com>
2018-12-15 01:22:39 +0300

    -

--- RecSystem/RecSystem/Models/IdentityDataInit.cs
+++ RecSystem/RecSystem/Models/IdentityDataInit.cs
@@ -7,54 +7,56 @@ namespace RecSystem.Models
 {
     public class IdentityDataInit
     {
-         public List<string> SeedUsers(UserManager<Customer> userManager)
+        public List<string> SeedUsers(UserManager<Customer> userManager)
         {
             List<string> usersId = new List<string>();
             try
             {
                 var engine = new FileHelperEngine<Cust>();
                 var records = engine.ReadFile("new_csv.csv");
-                            
+
 
                 foreach (var record in records)
                 {
                     if (userManager.FindByNameAsync(record.name).Result == null)
                     {
                         Customer user = new Customer();
-                        
-                        user.UserName = record.name;
-                        user.Name = record.name;
-                        user.Email = record.email;
+                        {
 
-                        IdentityResult result = userManager.CreateAsync
-                        (user, "Kc84XgQs!").Result;
+                            user.UserName = record.name;
+                            user.Name = record.name;
+                            user.Email = record.email;
 
-                        usersId.Add(userManager.GetUserIdAsync(user).Result);
+                            IdentityResult result = userManager.CreateAsync
+                            (user, "Kc84XgQs!").Result;
+
+                            usersId.Add(userManager.GetUserIdAsync(user).Result);
+                        }
                     }
-                    else {
-                    string ud = userManager.FindByNameAsync(record.name).Result.Id;
-                    usersId.Add(ud); 
-                     }
+                    else
+                    {
+                        string ud = userManager.FindByNameAsync(record.name).Result.Id;
+                        usersId.Add(ud);
+                    }
                 }
 
             }
             catch (Exception ex)
             {
-               
+
             }
             return usersId;
         }
     }
     [IgnoreEmptyLines]
     [DelimitedRecord(",")]
-    public class Cust 
-    {        
+    public class Cust
+    {
         public string age;
         public string sex;
         public string name;
         public string code;
         public string email;
-
-
     }
+
 }
