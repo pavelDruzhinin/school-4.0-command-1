805a017f5627c6dc57d6fc9b836bc7340d2db965
Aleksey <aleksandrov.a@roskvartal.ru>
2018-12-15 16:46:52 +0300

    Connection string

--- RecSystem/GetAdditionalInformation/Program.cs
+++ RecSystem/GetAdditionalInformation/Program.cs
@@ -12,7 +12,7 @@ namespace GetAdditionalInformation
         {
             var serviceProvider = new ServiceCollection()
                .AddLogging()
-               .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RecSystemDb;Trusted_Connection=True;MultipleActiveResultSets=true"), ServiceLifetime.Transient)
+               .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=tcp:recsystemdb.database.windows.net,1433;Initial Catalog=RecSystem;Persist Security Info=False;User ID=dbRecUser;Password=Hackaton2018;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"), ServiceLifetime.Transient)
                .AddTransient<AdditionalInfo>()
                .BuildServiceProvider();
 
---
+++ RecSystem/PushData/PPData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RecSystem.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+using RecSystem.Models;
+
+namespace PushData
+{
+    class PPData
+    {
+        private ApplicationDbContext _context;
+        private StreamReader sr;
+        private Item newItem;
+
+        public PPData(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void pushData()
+        {
+            string path="";
+            string line;
+            string[] itemData;
+            sr = new StreamReader(path, System.Text.Encoding.Default);
+            while((line=sr.ReadLine())!=null)
+            {
+              itemData=Regex.Split(line, "|");
+                foreach(var s in itemData)
+                {
+                    
+                }
+            }
+
+        }
+    }
+}
